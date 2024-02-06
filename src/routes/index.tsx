6d700fd6226b439628a97030784014f4e8b0b6df
morafde <morafde@yahoo.com>
2024-02-06 14:23:48 +0100

    Update index.tsx
    Changed Hello world text

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -5,15 +5,8 @@ export default function Home() {
   return (
     <main>
       <Title>Hello World</Title>
-      <h1>Hello world!</h1>
+      <h1>Site Under Construction, Please call 08033755957</h1>
       <Counter />
-      <p>
-        Visit{" "}
-        <a href="https://start.solidjs.com" target="_blank">
-          start.solidjs.com
-        </a>{" "}
-        to learn how to build SolidStart apps.
-      </p>
     </main>
   );
 }
